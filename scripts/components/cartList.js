e25a85c6f7a61414dcb987cc5373cf2512974fce
Dulces123 <jregaladoantonio@gmail.com>
2022-01-20 14:16:27 -0500

    add buttons to increase or decrease products quantity in shopping cart

--- scripts/components/cartList.js
+++ scripts/components/cartList.js
@@ -1,20 +1,39 @@
 import productAtCard from "./productAtCart.js";
-import DomHandler from "../domHandler.js"
-import STORE from "../store.js"
+import DomHandler from "../domHandler.js";
+import STORE from "../store.js";
 
 const cartList = (products) => {
   function cartOptions(e) {
-    const dropButton = e.target.closest('#drop')
-    const cart = e.target.closest('li')
-    if(dropButton){
-      const currentId = parseInt(cart.id);
-      const filteredProducts = STORE.getProductsAtCart().filter(product => product.id !== currentId);
-      STORE.setProductsAtCart(filteredProducts);
-      console.log(STORE.getProductsAtCart())
-      DomHandler.render(cartList(STORE.getProductsAtCart()),".cart-list");
-    }else{
+    const dropButton = e.target.closest("#drop");
+    const moreButton = e.target.closest("#more");
+    const lessButton = e.target.closest("#less");
+    const cart = e.target.closest("li");
+    const product = STORE.getProducts().find(
+      (product) => product.id === parseInt(cart.id)
+    );
+    let productQuantity = cart.querySelector("#product-quantity").innerText;
+    const basePrice = product.price * (1 - product.discount / 100);
 
+    if (dropButton) {
+      const filteredProducts = STORE.getProductsAtCart().filter(
+        (product) => product.id !== parseInt(cart.id)
+      );
+      STORE.setProductsAtCart(filteredProducts);
+      DomHandler.render(cartList(STORE.getProductsAtCart()), ".cart-list");
     }
+
+    if (moreButton) {
+      cart.querySelector("#product-quantity").innerText =
+        parseInt(productQuantity) + 1;
+    }
+
+    if (lessButton) {
+      if (cart.querySelector("#product-quantity").innerText == 1) return;
+      cart.querySelector("#product-quantity").innerText =
+        parseInt(productQuantity) - 1;
+    }
+    cart.querySelector("#product-price").innerText =
+      basePrice * parseInt(cart.querySelector("#product-quantity").innerText);
   }
 
   return {
--- scripts/components/productAtCart.js
+++ scripts/components/productAtCart.js
@@ -10,12 +10,12 @@ const productAtCard = (product) => {
       </div>
       <div>
         <p>${product.name}</p>
-        <p>${product.price}</p>
+        <p id = "product-price">${product.price}</p>
       </div>
       <div>
         <button><img id = "drop" class = "little-img" src = "../images/drop.png"></button>
         <button><img id = "less" class = "little-img"  src = "../images/less.png"></button>
-        <p>Quantity:</p><p>12</p>
+        <p>Quantity:</p><p id = "product-quantity" >1</p>
         <button><img id = "more" class = "little-img"  src = "../images/more.png"></button>
       </div>
       </li>
