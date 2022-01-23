642ce3e47db42a78e04d4295a19726d8528b7a66
Dulces123 <jregaladoantonio@gmail.com>
2022-01-22 19:34:45 -0500

    change url base to heroku app url

--- scripts/services/apiFetcher.js
+++ scripts/services/apiFetcher.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/";
+const BASE_URL = "https://mystorebackdulces.herokuapp.com/";
 const apiFetcher = async (endpoint) => {
   const response = await fetch(`${BASE_URL}${endpoint}`, { method: "GET" });
   if (!response.ok) {
